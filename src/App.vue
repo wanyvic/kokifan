885b9c541dfe74b86f7c56a0dce17cb8c30cee97
wanyvic <q873040807@gmail.com>
2019-09-26 01:00:59 +0800

    add

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,86 @@
 <template>
   <div id="app">
-    <router-view />
+    <TopContainer></TopContainer>
+    <BHeader></BHeader>
+    <BContent :rows="rows"></BContent>
+    <BNavSide :options="options" v-on:change="isShowMask"></BNavSide>
+    <div class="wnd-mask" ref="mask" v-show="showMask"></div>
   </div>
 </template>
 
 <script>
+import TopContainer from "./components/common/TopContainer";
+import BHeader from "./components/common/BHeader";
+import BContent from "./components/content/BContent";
+import BNavSide from "./components/nav/BNavSide";
+
+import { mapGetters } from "vuex";
 export default {
-  name: "App"
+  name: "App",
+  components: {
+    TopContainer,
+    BHeader,
+    BContent,
+    BNavSide
+  },
+  mounted() {
+    this.$store.dispatch("getContentRows");
+  },
+  data() {
+    return {
+      showMask: false
+    };
+  },
+  watch: {
+    options() {
+      console.log("options 变化了");
+    },
+    items() {
+      console.log("items 变化了");
+    }
+  },
+  computed: {
+    ...mapGetters(["requesting", "error", "rows"]),
+    options() {
+      let options = {
+        offset: 100, //偏移的距离
+        items: this.rows,
+        offsetTop: 0 //距离顶部距离
+      };
+      return options;
+    }
+  },
+  methods: {
+    isShowMask() {
+      this.showMask = !this.showMask;
+    }
+  }
 };
 </script>
 
-<style>
-body {
-  margin: 0;
-}
+<style lang="stylus">
 #app {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  height: 100vh;
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  font-family: 'Microsoft YaHei', Arial, Helvetica, sans-serif;
   -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  font-size: 12px;
+  margin: 0;
+  padding: 0;
+  background: #fff;
+  color: #222;
+  min-width: 990px;
+  tap-highlight-color: transparent;
+  -webkit-tap-highlight-color: transparent;
+
+  .wnd-mask {
+    position: fixed;
+    width: 100%;
+    height: 150%;
+    background-color: #000;
+    opacity: 0.5 !important;
+    z-index: 1000;
+    top: 0px;
+    left: 0px;
+    transition: 0.2s;
+  }
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -2,17 +2,17 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
 import App from './App.vue';
-import router from './router';
-import VueIpfs from './plugins/vue-ipfs';
+// import router from './router';
+// import VueIpfs from './plugins/vue-ipfs';
 Vue.config.productionTip = false;
 
 // Load our IPFS plugin.
-Vue.use(VueIpfs);
+// Vue.use(VueIpfs);
 /* eslint-disable no-new */
 
 Vue.config.productionTip = false;
 new Vue({
   render: h => h(App),
-  router,
+  // router,
   components: { App },
 }).$mount('#app');
